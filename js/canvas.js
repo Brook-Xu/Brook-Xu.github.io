a41d0f101035cab394150c5b207a6c8f4b49701f
Brook Xu <1308425295@qq.com>
2019-11-24 16:07:45 +0800

    add browser check

---
+++ js/canvas.js
@@ -0,0 +1,30 @@
+/* 
+* 1. 获取画布
+* 2. 获取画笔
+* 3. 直线，起止点
+* 4. 曲线，三点式
+* 5. 矩形，起止点
+* 6. 撤回，
+* 7. 跟踪鼠标变化
+*/
+
+window.onload = function () {
+    if (!this.checkBrowser()) {
+        alert('当前浏览器不支持canvas画布功能。请使用其他浏览器打开。推荐使用chrome浏览器。');
+        document.body.innerHTML = "当前浏览器不支持canvas画布功能。请使用其他浏览器打开。推荐使用chrome浏览器。";
+        return ;
+    }
+}
+
+var DrawCanvas = new function () {
+
+}
+
+// 检查浏览器的支持
+function checkBrowser () {
+    if (!FileReader) {
+        return false;
+    }else {
+        return true;
+    }
+}
